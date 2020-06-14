e80dba1bc101a650ecdc10bb52b569fbc821d684
jpvtillo <git@direlands.net>
2020-06-14 11:32:29 +0200

    Added service for high value transactions.

---
+++ transactionapps/src/JP.Demo.Chassis.HighValueNotification/Program.cs
@@ -0,0 +1,39 @@
+﻿using JP.Demo.Chassis.JaegerShared;
+using JP.Demo.Chassis.SharedCode.Kafka;
+using JP.Demo.Chassis.SharedCode.Schemas;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JP.Demo.Chassis.HighValueNotification
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var config = hostContext.Configuration;
+
+                    // Set up our tracing configuration. We expect this to be in env vars
+                    services.AddJaegerTracingForService(options =>
+                    {
+                        options.JaegerAgentHost = config["JAEGER_AGENT_HOST"];
+                        options.ServiceName = config["GLOBAL_NAME"];
+                    });
+
+                    // Set up our Kafka config
+                    services.Configure<KafkaConfig>(hostContext.Configuration.GetSection("KafkaConfig"));
+
+                    // Register services
+                    services.AddSingleton<KafkaConsumer<TransactionCreated>>();
+
+                    // Add our background worker
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ transactionapps/src/JP.Demo.Chassis.HighValueNotification/Worker.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using JP.Demo.Chassis.SharedCode.Kafka;
+using JP.Demo.Chassis.SharedCode.Schemas;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace JP.Demo.Chassis.HighValueNotification
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> logger;
+        private readonly KafkaConsumer<TransactionCreated> consumer;
+
+        public Worker(ILogger<Worker> logger, KafkaConsumer<TransactionCreated> consumer)
+        {
+            this.logger = logger;
+            this.consumer = consumer;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Yield(); // Avoid blocking entirely, see https://github.com/dotnet/runtime/issues/36063
+
+            await consumer.Consume(stoppingToken,
+                "TransactionCreated",
+                "highvaluenotification",
+                "TRANSACTIONS_HIGHVALUE",
+                async cr =>
+                {
+                    await ProcessMessage(cr);
+                });
+        }
+
+        private Task ProcessMessage(ConsumeResult<Ignore, TransactionCreated> cr)
+        {
+            var rq = cr.Message.Value;
+            logger.LogInformation($"High value transaction by '{rq.FromAccount}' (amount: {rq.AmountCents})'.");
+            return Task.CompletedTask;
+        }
+    }
+}
